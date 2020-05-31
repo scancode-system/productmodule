6ca945ebb7b4c4d692887945b594f861d2a218fd
Your Name <you@example.com>
2020-05-31 15:15:29 -0300

    .

--- Http/Controllers/Api/ProductController.php
+++ Http/Controllers/Api/ProductController.php
@@ -8,25 +8,36 @@ use Illuminate\Routing\Controller;
 use Modules\Product\Repositories\ProductRepository;
 use Modules\Product\Entities\Product;
 use Modules\Product\Events\ProductLazyEagerLoadingEvent;
+use Modules\Product\Events\ProductsLazyEagerLoadingEvent;
 
 class ProductController extends Controller
 {
 
     public function index()
     {
-        return ProductRepository::load(['id','barcode', 'sku', 'description', 'price', 'product_category_id'], ['product_category:id,description']);
+        $loads = event(new ProductsLazyEagerLoadingEvent());
+        $loads = collect($loads)->flatten()->toArray();
+        $loads = array_merge($loads, ['product_category:id,description']);
+
+        return ProductRepository::load(['id','barcode', 'sku', 'description', 'price', 'product_category_id'], $loads);
     }
 
     public function product(Request $request, Product $product)
     {
+
         $loads = event(new ProductLazyEagerLoadingEvent());
         $loads = collect($loads)->flatten()->toArray();
-    	$product->load($loads+['family']);
+
+    	$product->load(array_merge($loads, ['family', 'product_category'])); 
+
         foreach ($product->family as $product_family) {
-            $product_family->family_alias = $product_family->family_alias;     
+            $product_family->family_alias = $product_family->family_alias;
+            $product_family->product_category; 
+
+            $product_family->load($loads);
         }
-        //$product->family_alias = $product->family_alias;
-    	
+
+
         return $product;
     }
 
